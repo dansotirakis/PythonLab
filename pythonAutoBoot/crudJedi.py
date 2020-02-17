bcd38f73d334756e040b14132d622af85445866d
Damianos Sotirakis <damianos.sotirakis@zallpy.com>
2020-02-16 23:03:53 -0300

    ABCD-Jedi

--- pythonAutoBoot/crudJedi.py
+++ pythonAutoBoot/crudJedi.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8 -*-
 import shutil
 from six.moves import input
+from random import seed
+from random import random
 
 # --Variavles
 inputEntity = input("Entity   : ")
@@ -40,17 +43,21 @@ directoryApiEstablishmentMigration = directoryApiEstablishmentResource + "db/mig
 directoryApiEstablishmentTest = (
     directoryApiEstablishment + "test/" + directoryFolderEstablishment
 )
-directoryApiEstablishmentTestController = directoryFolderEstablishment + "controller/"
+directoryApiEstablishmentTestController = directoryApiEstablishmentTest + "controller/"
 
 # --CommonService
 directoryCommon = (
     directoryRootCode
     + "portaloficinas-common-objects/src/main/"
     + directoryFolder
-    + "/common/"
+    + "common/"
 )
 directoryDtoPromotion = directoryCommon + "dto/promotion/"
 directoryDtoEstablishment = directoryCommon + "dto/establishment/"
+dtoDirectory = directoryDtoPromotion + entityName + "DTO.java"
+dtoListDirectory = directoryDtoPromotion + entityName + "DTOList.java"
+valueGenerated1 = str(random()).replace(".", "")
+valueGenerated2 = str(random()).replace(".", "")
 
 
 def append_multiple_lines(file_name, lines_to_append):
@@ -75,7 +82,7 @@ def append_multiple_lines(file_name, lines_to_append):
             file_object.write(line)
 
 
-# -- Promotion ABCD method
+# -- ABCD methods promotion
 def create_promotion_be(en_name, migration, en_migration):
     # -- Migration
     print("Create migration: " + migration)
@@ -406,7 +413,375 @@ def create_promotion_be(en_name, migration, en_migration):
         newText = f.read().replace("deffault", en_name.lower())
     with open(directoryDtoPromotion + en_name + "DTO.java", "w") as fileLoop:
         fileLoop.write(newText)
+    # SerialVersionUID
+    with open(dtoDirectory) as f:
+        newText = f.read().replace("-98L", "-9" + valueGenerated1 + "8L")
+    with open(dtoDirectory, "w") as fileLoop:
+        fileLoop.write(newText)
+    with open(dtoListDirectory) as f:
+        newText = f.read().replace("-98L", "-9" + valueGenerated2 + "8L")
+    with open(dtoListDirectory, "w") as fileLoop:
+        fileLoop.write(newText)
+
+
+def create_establishment_be(en_name, migration, en_migration):
+    # -- Migration
+    print("Create migration: " + migration)
+    shutil.copyfile(
+        directoryResourceEstablishment + "XX_migration_default.sql",
+        directoryApiEstablishmentMigration + migration + ".sql",
+    )
+    with open(directoryApiEstablishmentMigration + migration + ".sql") as f:
+        newText = f.read().replace("deffault", en_migration)
+    with open(directoryApiEstablishmentMigration + migration + ".sql", "w") as fileLoop:
+        fileLoop.write(newText)
+    # -- Constant
+    constName = en_migration.replace("_", "-") + "s"
+    print("Create constant endPoint: " + en_migration.upper() + " to " + constName)
+    with open(directoryApiEstablishmentCode + "constant/ResourceName.java") as f:
+        newText = f.read().replace("}", " ")
+    with open(
+        directoryApiEstablishmentCode + "constant/ResourceName.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    lines = [
+        "  public static final String "
+        + en_migration.upper()
+        + ' = "'
+        + constName
+        + '"',
+        "",
+        "}",
+    ]
+    append_multiple_lines(
+        directoryApiEstablishmentCode + "constant/ResourceName.java", lines
+    )
+    # -- Message-i18n
+    print("Create Messages i18n")
+    with open(directoryApiEstablishmentCode + "i18n/Messages.java") as f:
+        newText = f.read().replace("}", " ")
+    with open(directoryApiEstablishmentCode + "i18n/Messages.java", "w") as fileLoop:
+        fileLoop.write(newText)
+    lines = [
+        "  public static final String ERROR_"
+        + en_migration.upper()
+        + '_NOT_FOUND ="ERROR_'
+        + en_migration.upper()
+        + '_NOT_FOUND";',
+        "  public static final String ERROR_"
+        + en_migration.upper()
+        + '_TITLE_CANNOT_BE_EMPTY = "ERROR_'
+        + en_migration.upper()
+        + '_TITLE_CANNOT_BE_EMPTY";',
+        "  public static final String ERROR_"
+        + en_migration.upper()
+        + '_TITLE_MAX_LIMIT_SIZE = "ERROR_'
+        + en_migration.upper()
+        + '_TITLE_MAX_LIMIT_SIZE";',
+        "  public static final String ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_CANNOT_BE_EMPTY =",
+        '      "ERROR_' + en_migration.upper() + '_DESCRIPTION_CANNOT_BE_EMPTY";',
+        "  public static final String ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_MAX_LIMIT_SIZE =",
+        '      "ERROR_' + en_migration.upper() + '_DESCRIPTION_MAX_LIMIT_SIZE";' "",
+        "}",
+    ]
+    append_multiple_lines(directoryApiEstablishmentCode + "i18n/Messages.java", lines)
+    # -- Message-es
+    print("Create message es: ")
+    with open(directoryApiEstablishmentResource + "message_es.properties") as f:
+        newText = f.read().replace("}", " ")
+    with open(
+        directoryApiEstablishmentResource + "message_es.properties", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    lines = [
+        "#",
+        "#" + en_name,
+        "#",
+        "ERROR_" + en_migration.upper() + "_NOT_FOUND=Registro no encontrado",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " el t\u00ectulo es obligatorio y debe llenarse",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " el t\u00ectulo debe tener menos de 50 caracteres",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " la descripci\u00f3n es obligatoria y debe completarse!",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " la descripci\u00f3n debe tener menos de 180 caracteres"
+        + "",
+        "}",
+    ]
+    append_multiple_lines(
+        directoryApiEstablishmentResource + "message_es.properties", lines
+    )
+    # -- Message-en
+    print("Create message en: ")
+    with open(directoryApiEstablishmentResource + "message.properties") as f:
+        newText = f.read().replace("}", " ")
+    with open(
+        directoryApiEstablishmentResource + "message.properties", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    lines = [
+        "#",
+        "#" + en_name,
+        "#",
+        "ERROR_" + en_migration.upper() + "_NOT_FOUND=Record not found",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " title is mandatory and must be filled",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " title must be less than 50 characters",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " description is mandatory and must be filled!",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " description must be less than 180 characters"
+        + "",
+        "}",
+    ]
+    append_multiple_lines(
+        directoryApiEstablishmentResource + "message.properties", lines
+    )
+    # -- Message-en
+    print("Create message pt: ")
+    with open(directoryApiEstablishmentResource + "message_pt.properties") as f:
+        newText = f.read().replace("}", " ")
+    with open(
+        directoryApiEstablishmentResource + "message_pt.properties", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    lines = [
+        "#",
+        "#" + en_name,
+        "#",
+        "ERROR_" + en_migration.upper() + "_NOT_FOUND=Registro n\u00E3o encontrado",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " T\u00EDtulo do "
+        + entityNameMessage.lower()
+        + " obrigat\u00F3rio!",
+        "ERROR_"
+        + en_migration.upper()
+        + "_TITLE_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " T\u00EDtulo do "
+        + entityNameMessage.lower()
+        + " deve ter menos de 50 caracteres",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_CANNOT_BE_EMPTY="
+        + entityNameMessage
+        + " Descri\u00E7\u00E3o do "
+        + entityNameMessage.lower()
+        + " \u00E9 obrigat\u00F3ria!",
+        "ERROR_"
+        + en_migration.upper()
+        + "_DESCRIPTION_MAX_LIMIT_SIZE="
+        + entityNameMessage
+        + " Descri\u00E7\u00E3o do "
+        + entityNameMessage.lower()
+        + " deve ter menos de 180 caracteres"
+        + "",
+        "}",
+    ]
+    append_multiple_lines(
+        directoryApiEstablishmentResource + "message_pt.properties", lines
+    )
+    # -- Model
+    print("Create model: " + en_name)
+    shutil.copyfile(
+        directoryResourceEstablishment + "ModelDefault.java",
+        directoryApiEstablishmentCode + "model/" + en_name + ".java",
+    )
+    with open(directoryApiEstablishmentCode + "model/" + en_name + ".java") as f:
+        newText = f.read().replace("deffault", en_migration)
+    with open(
+        directoryApiEstablishmentCode + "model/" + en_name + ".java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(directoryApiEstablishmentCode + "model/" + en_name + ".java") as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(
+        directoryApiEstablishmentCode + "model/" + en_name + ".java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(directoryApiEstablishmentCode + "model/" + en_name + ".java") as f:
+        newText = f.read().replace("DEFFAULT", en_name.upper())
+    with open(
+        directoryApiEstablishmentCode + "model/" + en_name + ".java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    # -- Repository
+    print("Create repository: " + en_name + "Repository")
+    shutil.copyfile(
+        directoryResourceEstablishment + "RepositoryDefault.java",
+        directoryApiEstablishmentCode + "repository/" + en_name + "Repository.java",
+    )
+    with open(
+        directoryApiEstablishmentCode + "repository/" + en_name + "Repository.java"
+    ) as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(
+        directoryApiEstablishmentCode + "repository/" + en_name + "Repository.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentCode + "repository/" + en_name + "Repository.java"
+    ) as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(
+        directoryApiEstablishmentCode + "repository/" + en_name + "Repository.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    # -- Service
+    print("Create service: " + en_name + "Service")
+    shutil.copyfile(
+        directoryResourceEstablishment + "ServiceDefault.java",
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java",
+    )
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java"
+    ) as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java"
+    ) as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java"
+    ) as f:
+        newText = f.read().replace("DEFFAULT", en_name.upper())
+    with open(
+        directoryApiEstablishmentCode + "service/" + en_name + "Service.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    # -- Controller
+    print("Create repository: " + en_name + "Repository")
+    shutil.copyfile(
+        directoryResourceEstablishment + "ControllerDefault.java",
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java",
+    )
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java"
+    ) as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java"
+    ) as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java"
+    ) as f:
+        newText = f.read().replace("DEFFAULT", en_migration.upper())
+    with open(
+        directoryApiEstablishmentCode + "controller/" + en_name + "Controller.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    # -- Test controller
+    print("Create controller test: " + en_name + "ControllerTest")
+    shutil.copyfile(
+        directoryResourceEstablishment + "ControllerTestDefault.java",
+        directoryApiEstablishmentTestController + en_name + "ControllerTest.java",
+    )
+    with open(
+        directoryApiEstablishmentTestController + en_name + "ControllerTest.java"
+    ) as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(
+        directoryApiEstablishmentTestController + en_name + "ControllerTest.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    with open(
+        directoryApiEstablishmentTestController + en_name + "ControllerTest.java"
+    ) as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(
+        directoryApiEstablishmentTestController + en_name + "ControllerTest.java", "w"
+    ) as fileLoop:
+        fileLoop.write(newText)
+    # -- DTO
+    print("Create DTOs: " + en_name + "DTO " + en_name + "ListDTO")
+    shutil.copyfile(
+        directoryResourceEstablishment + "DefaultDTO.java",
+        directoryDtoEstablishment + en_name + "DTO.java",
+    )
+    shutil.copyfile(
+        directoryResourceEstablishment + "DefaultListDTO.java",
+        directoryDtoEstablishment + en_name + "DTOList.java",
+    )
+    with open(directoryDtoEstablishment + en_name + "DTOList.java") as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(directoryDtoEstablishment + en_name + "DTOList.java", "w") as fileLoop:
+        fileLoop.write(newText)
+    with open(directoryDtoEstablishment + en_name + "DTO.java") as f:
+        newText = f.read().replace("Deffault", en_name)
+    with open(directoryDtoEstablishment + en_name + "DTO.java", "w") as fileLoop:
+        fileLoop.write(newText)
+    with open(directoryDtoEstablishment + en_name + "DTOList.java") as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(directoryDtoEstablishment + en_name + "DTOList.java", "w") as fileLoop:
+        fileLoop.write(newText)
+    with open(directoryDtoEstablishment + en_name + "DTO.java") as f:
+        newText = f.read().replace("deffault", en_name.lower())
+    with open(directoryDtoEstablishment + en_name + "DTO.java", "w") as fileLoop:
+        fileLoop.write(newText)
+    # SerialVersionUID
+    with open(dtoDirectory) as f:
+        newText = f.read().replace("-98L", "-9" + valueGenerated1 + "8L")
+    with open(dtoDirectory, "w") as fileLoop:
+        fileLoop.write(newText)
+    with open(dtoListDirectory) as f:
+        newText = f.read().replace("-98L", "-9" + valueGenerated2 + "8L")
+    with open(dtoListDirectory, "w") as fileLoop:
+        fileLoop.write(newText)
 
 
 # -- ABCD
-create_promotion_be(entityName, migrationName, entityMigration)
+inputOption = input("API Promotion (1) Establishment (2) : ")
+if inputOption == "1":
+    create_promotion_be(entityName, migrationName, entityMigration)
+elif inputOption == "2":
+    create_establishment_be(entityName, migrationName, entityMigration)
